4c6c05b4c9231e901dfffe9031938b2058f1bf34
Sebastian Ehlert <28669218+awvwgk@users.noreply.github.com>
2022-03-12 09:25:11 +0100

    Fix cross build with OpenMPI

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 set -ex
 
+if [ "${mpi}" == "openmpi" ]; then
+    export OPAL_PREFIX=$PREFIX
+    export OMPI_MCA_plm=isolated
+    export OMPI_MCA_btl_vader_single_copy_mechanism=none
+    export OMPI_MCA_rmaps_base_oversubscribe=yes
+fi
+
 cmake_options=(
    "-DCMAKE_INSTALL_PREFIX=${PREFIX}"
    "-DCMAKE_INSTALL_LIBDIR=lib"
@@ -15,6 +22,7 @@ mkdir -p _build
 pushd _build
 cmake ${CMAKE_ARGS} "${cmake_options[@]}"
 
-ninja all install
+cmake --build .
+cmake --install .
 
 popd
